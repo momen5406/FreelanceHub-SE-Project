88e6bb13b0aad4b98327da561c3abb61ac2d9db9
motazahmed1 <106410512+motazahmed1@users.noreply.github.com>
2026-05-07 20:54:32 +0300

    Delete test.php

--- public/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/models/MarketplaceHealth.php';
-
-$health = new MarketplaceHealth();
-$metrics = $health->getDashboardMetrics();
-
-echo "<pre>";
-print_r($metrics);
-echo "</pre>";
